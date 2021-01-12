df0653989b93d8f9d7d0c7a54b853d5e012808e8
Alex Poon <chpoon92@tutanota.com>
2021-01-12 17:48:45 +0800

    Add chocolatey setup

---
+++ choco.ps1
@@ -0,0 +1,18 @@
+# tools
+choco install 7zip -y
+choco install firefox -y
+choco install icue -y
+choco install logitech-options -y
+
+# dev
+choco install cuda -y
+choco install docker-desktop -y
+choco install intellijidea-ultimate -y
+choco install vagrant -y
+choco install wsl-ubuntu-2004 -y
+
+# entertainment
+choco install origin -y
+choco install steam -y
+choco install uplay -y
+choco install vortex -y

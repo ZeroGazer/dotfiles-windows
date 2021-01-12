ef4a32f6134970394302d009b4e21555146635f8
Alex Poon <chpoon92@tutanota.com>
2021-01-12 17:47:54 +0800

    Add windows feature setup

---
+++ windows-feature.ps1
@@ -0,0 +1,3 @@
+Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Hyper-v -All
+Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Windows-Subsystem-Linux -All
+Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName VirtualMachinePlatform -All
